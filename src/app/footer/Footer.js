a2a89aaa366891c70db17f0ad18daf9f57bde663
edwinauliano <edwinauliano@gmail.com>
2022-05-03 18:52:04 +0700

    remove unuse code

--- src/app/footer/Footer.js
+++ src/app/footer/Footer.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Logo from '../common/asset/logo-grabfood-mono.svg';
-import Grid from '@mui/material/Grid';
 import { FooterNew } from './footer-new/FooterNew';
 import { FooterDark } from './footer-dark/FooterDark';
 
