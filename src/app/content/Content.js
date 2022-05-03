019e0c4b13fe048cdbf38f33fe6ab69d828b9485
edwinauliano <edwinauliano@gmail.com>
2022-05-03 18:49:21 +0700

    add popup lodal lihat detail

--- src/app/content/Content.js
+++ src/app/content/Content.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import Stack from '@mui/material/Stack';
@@ -9,6 +9,8 @@ import LogoStart from '../common/asset/icon-star.svg';
 import LogoPromo from '../common/asset/icon-promo-tag.svg';
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 import { Menu } from './Menu';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 import './content.css';
 
 function handleClick(event) {
@@ -16,10 +18,31 @@ function handleClick(event) {
     console.info('You clicked a breadcrumb.');
   }
 
-
 export const Content = () => {
 
     const jamBuka = 'Hari ini  Closed';
+    const [open, setOpen] = useState(false);
+
+
+    const handleLihatDetail = () => {
+      setOpen(true);
+    }
+
+    const handleClose = () => {
+       setOpen(false);
+    }
+
+    const style = {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: 400,
+      bgcolor: 'background.paper',
+      border: '0px solid #  000',
+      boxShadow: 24,
+      p: 4,
+    };
     
     const breadcrumbs = [
         <Link underline="hover" key="1" color="inherit" href="#" onClick={handleClick}>
@@ -52,12 +75,28 @@ export const Content = () => {
             <h3 className='cuisine'>Hidangan Laut,Cemilan</h3>
             <img src={LogoStart}/><p className='rating'>4.5 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 554,3 km</p>
             <p className='label-open'>Jam Buka</p><p className='open-store'>{jamBuka}</p><p className='close-style'>Tutup</p>
-            <img src={LogoPromo}/><p className='title-promo'>HEMAT80G 80rb | HEMAT30G 30%</p><p className='detail-promo'>Lihat Detail</p>
+            <img src={LogoPromo}/><p className='title-promo'>HEMAT80G 80rb | HEMAT30G 30%</p><p className='detail-promo' onClick={handleLihatDetail}>Lihat Detail</p>
             <InfoOutlinedIcon fontSize='medium'></InfoOutlinedIcon> <p className='info-promo'>Biaya pemesanan senilai Rp3.000 berlaku di restoran ini.</p>
         </div>
         <div className='menu'>
             <Menu></Menu>
         </div>
+
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+          <strong>Promosi <img className='logo-modal' src={LogoPromo}/></strong>
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 4 }}>
+            <strong>HEMAT80G 80rb | HEMAT30G 30%</strong>
+          </Typography>
+        </Box>
+        </Modal>
     </div>
   )
 }
